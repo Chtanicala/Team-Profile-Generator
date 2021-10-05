cc7f73772869747994d65916aa73edbea7925a49
Christian Tanicala <christian.tanicala@gmail.com>
2021-10-04 20:21:14 -0400

    Starting to run tests

---
+++ tests/Employee.test.js
@@ -0,0 +1,9 @@
+const Employee = require("../lib/Employee.js")
+
+describe('Name', () => {
+    it('Should return a specified name', () => {
+        const employee1 = new Employee('Jake')
+
+        expect(employee1.name).toBe('Jake');
+    })
+});
