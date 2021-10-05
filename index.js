3f2c57e54270ac22f0a0cdbfb756d7196d3a033a
Christian Tanicala <christian.tanicala@gmail.com>
2021-10-05 00:07:40 -0400

    Updated index.js w/ questions arrays

--- index.js
+++ index.js
@@ -1,3 +1,105 @@
 const inquirer = require("inquirer");
 const fs = require("fs")
 const jest = require("jest")
+
+const Employee = require('./lib/Employee')
+const Manager = require('./lib/Manager')
+const Intern = require('./lib/Intern')
+const Engineer = require('./lib/Engineer')
+
+const managerQuestions = [
+    {
+        type: 'input',
+        message: "Please input manager's name.",
+        name: "name"
+    },
+
+    {
+        type: 'input',
+        message: "Please input manager's employee ID.",
+        name: "id"
+    },
+
+    {
+        type: 'input',
+        message: "Please input manager's email.",
+        name: "email"
+    },
+    {
+        type: 'input',
+        message: "Please input manager's office number.",
+        name: "officeNumber"
+    }
+]
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        message: "Please input engineer's name.",
+        name: "name"
+    },
+
+    {
+        type: 'input',
+        message: "Please input engineer's employee ID.",
+        name: "id"
+    },
+
+    {
+        type: 'input',
+        message: "Please input engineer's email.",
+        name: "email"
+    },
+    {
+        type: 'input',
+        message: "Please input engineer's github username.",
+        name: "github"
+    }
+]
+
+const internQuestions = [
+    {
+        type: 'input',
+        message: "Please input intern's name.",
+        name: "name"
+    },
+
+    {
+        type: 'input',
+        message: "Please input intern's employee ID.",
+        name: "id"
+    },
+
+    {
+        type: 'input',
+        message: "Please input intern's email.",
+        name: "email"
+    },
+    {
+        type: 'input',
+        message: "Please input engineer's school.",
+        name: "school"
+    }
+]
+
+
+inquirer
+  .prompt([
+    {
+        type: "input",
+        message: "Enter a title for this project.",
+        name: "Project Title",
+    },
+  ])
+  .then((answers) => {
+    console.log(answers);
+    let output = 
+    fs.writeFile("./SampleREADME.md", output, ()=>{});
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
