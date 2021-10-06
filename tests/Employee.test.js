7cfc5f12525784f265e5d13b164c3606d197ccab
Christian Tanicala <christian.tanicala@gmail.com>
2021-10-05 21:48:18 -0400

    Successful Employee.test.js

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -7,3 +7,27 @@ describe('Name', () => {
         expect(employee1.getName()).toBe('Jake');
     })
 });
+
+describe('Id', () => {
+    it('Should return a specified id', () => {
+        const employee1 = new Employee("Jake", 4)
+
+        expect(employee1.getId()).toBe(4);
+    })
+});
+
+describe('Email', () => {
+    it('Should return a specified email', () => {
+        const employee1 = new Employee("Jake", 4, "christian.tanicala@gmail.com")
+
+        expect(employee1.getEmail()).toBe("christian.tanicala@gmail.com");
+    })
+});
+
+describe('Role', () => {
+    it('Should return "Employee"', () => {
+        const employee1 = new Employee()
+
+        expect(employee1.getRole()).toBe("Employee");
+    })
+});
