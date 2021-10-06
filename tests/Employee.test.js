ecd2a9162a6548b75f412408653b4c345e797676
Christian Tanicala <christian.tanicala@gmail.com>
2021-10-05 21:41:24 -0400

    jest working

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -4,6 +4,6 @@ describe('Name', () => {
     it('Should return a specified name', () => {
         const employee1 = new Employee('Jake')
 
-        expect(employee1.name).toBe('Jake');
+        expect(employee1.getName()).toBe('Jake');
     })
 });
